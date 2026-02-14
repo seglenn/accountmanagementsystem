9fde132b338930b6f695e912f85353d3c2e3fe1d
John <ianmagtuba19@gmail.com>
2026-02-14 17:32:34 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AccountManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Account Management System");
+
+            string username = "glennlambert";
+            string password = "gnnlmbrt123!";
+
+            Console.WriteLine("Enter Username: ");
+            string usernameinput = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            string passwordinput = Console.ReadLine();
+
+            if (usernameinput == username && passwordinput == password)
+            {
+                Console.WriteLine("Log in Successfull");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect inputs");
+
+            }
+        }
+    }
+}
